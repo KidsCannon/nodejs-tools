9054773ceca3410170f3984f366ad5b718228a48
Takatoshi Matsumoto <toqoz403@gmail.com>
2022-04-07 15:48:20 +0900

    add readline-async

---
+++ packages/readline-async/lib/index.ts
@@ -0,0 +1,10 @@
+import readline, { ReadLineOptions } from 'readline'
+
+export const readlineAsync = async (opts: ReadLineOptions, fn: (input: string) => void) => {
+  return new Promise((resolve, reject) => {
+    readline.createInterface(opts)
+      .on('line', fn)
+      .on('error', reject)
+      .on('close', resolve)
+  })
+}
