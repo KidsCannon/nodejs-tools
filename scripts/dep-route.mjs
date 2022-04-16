58bf5d4d3dbe50c726534c64cb9284febe7d239e
Takatoshi Matsumoto <toqoz403@gmail.com>
2022-04-16 10:56:28 +0900

    scripts: add dep-route.mjs
    $ npx zx scripts/dep-route.mjs async
    async@3.2.3 <- archiver@5.3.0 <- testcontainers@8.6.1 <- [devDependencies] <- packages/aws-s3-cp@0.0.4

---
+++ scripts/dep-route.mjs
@@ -0,0 +1,36 @@
+import fs from 'fs/promises'
+import path from 'path'
+
+$.verbose = false
+
+const format = (packageLockJson, target) => {
+  const p = target.startsWith('packages/') ? packageLockJson.packages[target] : packageLockJson.packages[`node_modules/${target}`]
+  return `${target}@${p.version}`
+}
+
+const depRoute = (packageLockJson, target, result = []) => {
+  for (const pkgKey of Object.keys(packageLockJson.packages)) {
+    const pkg = packageLockJson.packages[pkgKey]
+    const pkgName = pkgKey.replace(/^node_modules\//, '')
+    if (pkg.dependencies?.[target] != null) {
+      result.push(format(packageLockJson, pkgName))
+      return depRoute(packageLockJson, pkgName, result)
+    } else if (pkg.devDependencies?.[target] != null) {
+      result.push('[devDependencies]')
+      result.push(format(packageLockJson, pkgName))
+      return depRoute(packageLockJson, pkgName, result)
+    }
+  }
+  return result
+}
+
+const target = process.argv[3]
+if (target == null) {
+  console.log(`Usage: $ npx zx scripts/dep-route.mjs <dep>`)
+  process.exit(1)
+}
+
+const packageLockJsonPath = path.join(__dirname, '..', 'package-lock.json')
+const packageLockJson = JSON.parse((await fs.readFile(packageLockJsonPath)).toString('utf-8'))
+const result = depRoute(packageLockJson, target, [format(packageLockJson, target)])
+console.log(result.join(' <- '))
